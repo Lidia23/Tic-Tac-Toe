4263082cba984c324672a0148d055b1e43d7e2ab
Lidia Qama <lidia22qama@gmail.com>
2024-02-04 16:48:39 +0100

    disable button if it is clicked once commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import Player from "./components/Player"
 import GameBoard from "./components/GameBoard";
 import { useState } from "react";
 import Log from "./components/Log";
-
+const deriveActivePlayer = (gameTurns) => {
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  // const [activePlayer, setActivePlayer] = useState('X');
+ const activePlayer = deriveActivePlayer(gameTurns);
   const handleSelectSquare = (rowIndex, colIndex) => {
-    setActivePlayer(curActivePlayer => curActivePlayer === 'X' ? 'O' : 'X');
+    // setActivePlayer(curActivePlayer => curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns(prevTurns => {
-      let currentPlayer = 'X';
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
       const updateTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer },
       ...prevTurns];
       return updateTurns;
@@ -28,7 +32,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns}/>
     </main>
   )
 }
